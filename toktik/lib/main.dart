0ec23196379bd26ae24487ae9bfc0e85ebf76607
samu7861 <sfrancog7861@gmail.com>
2024-05-11 12:14:19 -0500

    DiscoverProvidernMultiProvider

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -9,11 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      home: const DiscoverScreen()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create:(_) => DiscoverProvider())
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const DiscoverScreen()
+      ),
     );
   }
 }
