1c51abec25da55aef718bb63c59b20d7314b2222
samu7861 <sfrancog7861@gmail.com>
2024-05-18 09:43:07 -0500

    VideoButton

--- toktik/lib/presentation/widget/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widget/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widget/shared/video_button.dart';
 
 class VideoScrollableView extends StatelessWidget {
 
@@ -9,13 +10,31 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container( color: Colors.red ),
-        Container( color: Colors.blue ),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final videoPost = videos[index];
+
+        return Stack(
+
+          children: [
+
+            //video player + gradiente
+
+
+            // Botones
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+              )
+
+          ],
+
+        );
+      },
     );
   }
 }
