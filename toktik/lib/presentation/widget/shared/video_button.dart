8aa1e4a8c09b9a35241580a9a30eb22416325ed5
samu7861 <sfrancog7861@gmail.com>
2024-05-18 11:55:12 -0500

    Spinning button

--- toktik/lib/presentation/widget/shared/video_button.dart
+++ toktik/lib/presentation/widget/shared/video_button.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class VideoButtons extends StatelessWidget {
@@ -12,7 +14,14 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox(height: 20),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20,),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(value: 0, iconData: Icons.play_circle_outline)
+          )
       ],
     );
   }
@@ -34,7 +43,8 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: () {} , 
           icon: Icon(iconData, color: color,), 
           ),
-        Text("$value")
+        if (value > 0)  
+        Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ],
     );
   }
